34da9c61d77e380e67fe426d72c007c32bad2d3c
kraemr <93094049+kraemr@users.noreply.github.com>
2026-07-20 16:08:43 +0200

    help

--- src/subdevice_group/mod.rs
+++ src/subdevice_group/mod.rs
@@ -1345,7 +1345,7 @@ where
         // time is rounded to a whole number of `sync0_period`-length cycles.
         let cycle_start_offset = time % maindevice.config.cycle_time;
         let time_to_next_iter =
-            (maindevice.cycle_time - cycle_start_offset);
+            (maindevice.config.cycle_time - cycle_start_offset);
         Ok(TxRxResponse {
             working_counter: lrw_wkc_sum,
             subdevice_states,
